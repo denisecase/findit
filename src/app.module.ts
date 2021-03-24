f7619280990a5074d287aed93790378a168c608d
Denise Case <denisecase@gmail.com>
2021-03-24 02:13:23 -0500

    working on prod db connection errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,26 +6,46 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LocationModule } from './location/location.module';
 
+const optionsDevelopment = {
+  dialect: 'sqlite',
+  storage: './dev.sqlite',
+  autoLoadModels: true,
+  synchronize: true,
+}
+
+const optionsProduction = {
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  database: process.env.DB_PRODUCTION,
+  username: process.env.DB_USER,
+  port: parseInt(process.env.DB_PORT),
+  password: process.env.DB_PASS,
+  autoLoadModels: true,
+  synchronize: true,
+}
+
+const sequelizeOptions = process.env.NODE_ENV === 'production'
+  ? optionsProduction : optionsDevelopment;
+;
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env'
     }),
-    SequelizeModule.forRoot({
-      dialect: (process.env.NODE_ENV == 'dev') ? 'sqlite' : 'postgres',
-      storage: (process.env.NODE_ENV == 'dev') ? './dev.sqlite' : process.env.DATABASE_URL,
-      ssl: (process.env.NODE_ENV == 'dev') ? false : true,
-      dialectOptions: {
-        "ssl": {"require":(process.env.NODE_ENV == 'dev') ? false : true }
-      },
-      database: process.env.DB_DATABASE,
-      password: process.env.DB_PASSWORD,
-      host: process.env.DB_HOSTNAME || 'localhost',
-      port: parseInt(process.env.DB_PORT ) || 5432,
-      autoLoadModels: true,
-      synchronize: true,
-    }),
+    SequelizeModule.forRoot(
+      {
+        dialect: process.env.NODE_ENV === 'production' ? 'postgres' : 'sqlite',
+        host: process.env.NODE_ENV === 'production' ? process.env.DB_HOST : 'localhost',
+        storage: './dev.sqlite',
+        database: process.env.DB_PRODUCTION,
+        username: process.env.DB_USER,
+        port: parseInt(process.env.DB_PORT),
+        password: process.env.DB_PASS,
+        autoLoadModels: true,
+        synchronize: true,
+      }
+    ),
     LocationModule,
     SeederModule.forRoot({
       runOnlyIfTableIsEmpty: false,
